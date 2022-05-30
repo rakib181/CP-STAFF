dff11cf8f1addaebb17bab468195481e65d13f6e
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-05-30 19:08:15 +0600

    Update Binary Search.cpp

--- Binary Search/Binary Search.cpp
+++ Binary Search/Binary Search.cpp
@@ -39,3 +39,52 @@ int32_t main(){
     cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
     return 0;
 }
+
+
+//Another Approach
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int binary(vector<int> v,int n,int x){
+  int l=-1,r=n;
+  while(r>l+1){
+    int mid=(l+r)/2;
+    if(v[mid] < x){
+      l=mid;
+    }else{
+      r=mid;
+    }
+  }
+  if(r<n && v[r]==x){
+    cout<<"YES\n";
+  }else{
+    cout<<"NO\n";
+  }
+}
+
+int32_t main(){
+   ios_base::sync_with_stdio(0);
+    cin.tie(0); 
+    cout.tie(0);
+    #ifndef ONLINE_JUDGE 
+    freopen("input.txt","r",stdin); 
+    freopen("output.txt","w",stdout); 
+    #endif
+    auto start=clock();
+    int n ,q; 
+    cin >> n >> q;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+      cin>>v[i];
+    }
+    while(q--){
+      int x;
+      cin>>x;
+     binary(v,n,x);
+    }
+   
+    cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
+    return 0;
+}
+
