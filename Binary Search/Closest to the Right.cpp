08797941f613dd2b2aa6f828c2eddd572328ff0a
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-05-30 10:38:23 +0600

    Create Closest to the Right.cpp

---
+++ Binary Search/Closest to the Right.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int binarySearch(int linear[],int n,int x){
+  int l=-1,r=n;
+     while(r>l+1){
+     int mid =(l+r)/2;
+     if(linear[mid]<x){
+      l=mid;
+     }else{
+       r=mid;
+     } 
+    }
+    return r+1;
+}
+int32_t main(){
+   ios_base::sync_with_stdio(0);
+    cin.tie(0); 
+    cout.tie(0);
+    #ifndef ONLINE_JUDGE 
+    freopen("input.txt","r",stdin); 
+    freopen("output.txt","w",stdout); 
+    #endif
+    auto start=clock();
+    int n,q;
+    cin>>n>>q;
+    int linear[n];
+    for(int i=0;i<n;i++){
+      cin>>linear[i];
+    }
+    while(q--){
+    int x;cin>>x;
+    int ans = binarySearch(linear,n,x);
+    cout<<ans<<'\n';
+  }
+    cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
+    return 0;
+}
