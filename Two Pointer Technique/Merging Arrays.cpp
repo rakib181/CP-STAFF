7046ebef4d8eefd10d0fda1f729af8deb3b8e705
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-06-01 10:34:43 +0600

    Create Merging Arrays.cpp

---
+++ Two Pointer Technique/Merging Arrays.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int32_t main(){
+   ios_base::sync_with_stdio(0);
+    cin.tie(0); 
+    cout.tie(0);
+    #ifndef ONLINE_JUDGE 
+    freopen("input.txt","r",stdin); 
+    freopen("output.txt","w",stdout); 
+    #endif
+    auto start=clock();
+    int n,m;
+    cin>>n >> m;
+    int a[n];
+     int b[m];
+    for(int i=0;i<n;i++){
+      cin>>a[i];
+    }
+    for(int i=0;i<m;i++){
+      cin>>b[i];
+    }
+    int merge[n+m];
+    int i=0,j=0,k=0;
+    while(i<n && j<m){
+      if(a[i]>b[j]){
+        merge[k++]=b[j++];
+      }else{
+        merge[k++]=a[i++];
+      }
+    }
+    while(i<n){
+      merge[k++]=a[i++];
+    }
+    while(j<m){
+      merge[k++]=b[j++];
+    }
+    for(int i=0;i<n+m;i++){
+      cout<<merge[i]<<" ";
+    }
+    
+    cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
+    return 0;
+}
