06591f65c7a8e1838b6b18189c1c50b51c1529be
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-06-01 12:41:59 +0600

    Update Number of Smaller.cpp

--- Two Pointer Technique/Number of Smaller.cpp
+++ Two Pointer Technique/Number of Smaller.cpp
@@ -36,3 +36,42 @@ int32_t main(){
     cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
     return 0;
 }
+
+// Another Approach
+#include<bits/stdc++.h>
+using namespace std;
+
+int32_t main(){
+   ios_base::sync_with_stdio(0);
+    cin.tie(0); 
+    cout.tie(0);
+    #ifndef ONLINE_JUDGE 
+    freopen("input.txt","r",stdin); 
+    freopen("output.txt","w",stdout); 
+    #endif
+    auto start=clock();
+    int n,m;
+    cin>> n >> m;
+   vector<int> a(n);
+   vector<int> b(m);
+    for(int i=0;i<n;i++){
+      cin>> a[i];
+    }
+    for(int i=0;i<m;i++){
+      cin>> b[i];
+    }
+   vector<int> res(m);
+    int i=0;
+    for(int j=0;j<m;j++){
+      while(i<n && a[i]<b[j]){
+        i++;
+      }
+      res[j]=i;
+    }    
+    for(int x : res){
+      cout<< x <<" ";
+    }
+    cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
+    return 0;
+}
+
