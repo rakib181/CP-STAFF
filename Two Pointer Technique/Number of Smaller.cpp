98c8be69977874e0bb77e991d368c52b386f2890
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-06-01 12:34:27 +0600

    Create Number of Smaller.cpp

---
+++ Two Pointer Technique/Number of Smaller.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int32_t main(){
+   ios_base::sync_with_stdio(0);
+    cin.tie(0); 
+    cout.tie(0);
+    #ifndef ONLINE_JUDGE 
+    freopen("input.txt","r",stdin); 
+    freopen("output.txt","w",stdout); 
+    #endif
+    auto start=clock();
+    int n,m;
+    cin>> n >> m;
+   vector<int> a(n);
+   vector<int> b(m);
+    for(int i=0;i<n;i++){
+      cin>> a[i];
+    }
+    for(int i=0;i<m;i++){
+      cin>> b[i];
+    }
+   vector<int> res(m);
+    int i=0,j=0,k=0;
+    while(i<n || j<m){
+      if(j==m || (i<n && a[i]<b[j])){
+        k++;i++;
+      }else{
+        res[j]=i;
+       k++;j++;
+      }
+    }
+    for(int x : res){
+      cout<< x <<" ";
+    }
+    cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
+    return 0;
+}
