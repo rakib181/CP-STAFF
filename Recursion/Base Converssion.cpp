81f00fd9a28bd81c0e1645a22d73e563da474c5f
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-08-08 21:03:34 +0600

    Create Base Converssion.cpp

---
+++ Recursion/Base Converssion.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void ok(int n){
+    if(n == 0)return;
+
+    int x = n % 2;
+    ok(n / 2);
+    cout << x;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(0);
+    int tt;
+    cin >> tt;
+    while(tt--) {
+        int n;
+        cin >> n;
+        if(n == 0){
+            cout << "0" << '\n';
+            continue;
+        }
+        ok(n);
+        cout << '\n';
+    }
+    return 0;
+}
