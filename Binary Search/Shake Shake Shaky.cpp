b4220c577caeb8ebe483eea8bcf1d832666e54d9
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-08-25 14:03:12 +0600

    Update Shake Shake Shaky.cpp

--- Binary Search/Shake Shake Shaky.cpp
+++ Binary Search/Shake Shake Shaky.cpp
@@ -1,50 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
-const int N=50010;
-int ar[N];
-ll n, k;
 
-long long binary(long long l,long long r){
-  ll mid, maxi=0, cnt;
-  while(l<=r){
-    mid=(l+r)>>1;
-    cnt=0;
-    for(int i=0; i<n; i++){
-      cnt+= ar[i] / mid;
-      if(cnt >= k)break;
+bool good(vector<int> &ar, int n, long long k, long long x){
+    long long cnt = 0;
+    for(int i = 0; i < n; i++){
+        cnt += (ar[i] / x);
+        if(cnt >= k)return true;
     }
-    if(cnt>=k){
-      l = mid + 1;
-      maxi=max(maxi,mid);
-    }else{
-      r = mid - 1;
-    }
-  }
-  return maxi;
+    return false;
 }
 
-
 int32_t main(){
-   ios_base::sync_with_stdio(0);
-    cin.tie(0); 
-    cout.tie(0);
-    #ifndef ONLINE_JUDGE 
-    freopen("input.txt","r",stdin); 
-    freopen("output.txt","w",stdout); 
-    #endif
-    auto start=clock();
-    int tt;
-    cin >> tt;
-    while(tt--){
+     ios_base::sync_with_stdio(0);
+      cin.tie(0); 
+      #ifndef ONLINE_JUDGE 
+      freopen("input.txt","r",stdin); 
+      freopen("output.txt","w",stdout); 
+      #endif
+      auto start=clock();
+      int tt;
+      cin >> tt;
+      while(tt--){
+      long long n, k;
       cin >> n >> k;
-      ll maxi=-1;
-      for(int i = 0 ; i < n ; i++){
+      vector<int> ar(n);
+      for(int i = 0; i < n; i++){
         cin >> ar[i];
-        maxi=max((int)maxi,(int)ar[i]);
       }
-     cout << binary(1,maxi) << '\n';
-    }
-    cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
-    return 0;
+      long long l = 1, r = *max_element(ar.begin(), ar.end()), ans = 0;
+      while(l <= r){
+        long long mid  = (l + r) >> 1;
+        if(good(ar, n, k, mid)){
+            ans = max(ans, mid);
+            l = mid + 1;
+        }else r = mid - 1;
+      }
+      cout << ans << '\n';
+      }
+      cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
+      return 0;
 }
