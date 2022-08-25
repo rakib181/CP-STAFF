3d6a02a23da0a0ab5a8fb67b7086b2500be58f72
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-08-25 14:20:38 +0600

    Update K-th Not Divisible by n.cpp

--- Binary Search/K-th Not Divisible by n.cpp
+++ Binary Search/K-th Not Divisible by n.cpp
@@ -1,48 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-long long n,k;
-
-bool good1(long long x){
-  long long div= x - (x/n);
-  return div > k;
-}
-
-bool good2(long long x){
-  long long div= x - (x/n);
-  return div < k;
+long long good(long long n, long long x){
+    long long res = x - (x / n);
+    return res;
 }
 
 int32_t main(){
-   ios_base::sync_with_stdio(0);
-    cin.tie(0); 
-    cout.tie(0);
-    #ifndef ONLINE_JUDGE 
-    freopen("input.txt","r",stdin); 
-    freopen("output.txt","w",stdout); 
-    #endif
-    auto start=clock();
-    long long tt;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt;
     cin >> tt;
     while(tt--){
-    cin>>n>>k;
-    long long l=1,r=1e18;
-    while(l<=r){
-      long long mid=(l+r)>>1;
-      if(good1(mid)){
-        r=mid-1;
-      }else if(good2(mid)){
-        l=mid+1;
-      }else{
-        if(mid % n != 0){
-          cout << mid << '\n';
-          break;
-        }else{
-         r=mid-1;
+    long long n, k;
+    cin >> n >> k;
+    long long l = 0, r = n * k;
+    while(l <= r){
+        long long mid = (l + r) >> 1;
+        if(good(n, mid) == k && mid % n){
+        cout << mid << '\n';
+        break;
         }
+        if(good(n, mid) < k){
+          l = mid + 1;
+        }else r = mid - 1;
       }
     }
-  }
-    cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
     return 0;
 }
