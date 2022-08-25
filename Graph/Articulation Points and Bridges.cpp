9e0dcfa68cca8d856150ae69bf236d30e004c891
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-08-25 21:23:16 +0600

    Update Articulation Points and Bridges.cpp

--- Graph/Articulation Points and Bridges.cpp
+++ Graph/Articulation Points and Bridges.cpp
@@ -60,3 +60,70 @@ int32_t main(){
     cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
     return 0;
 }
+
+
+
+//LatsTry Again
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 100;
+vector<int> g[N];
+vector<int> low(N, 0), dis(N, 0);
+vector<bool> vis(N, false);
+vector<pair<int, int>> bridges;
+set<int> art_points;
+int timer = 1;
+
+void dfs(int cur, int par){
+    vis[cur] = true;
+    dis[cur] = low[cur] = timer++;
+    int child = 0;
+    for(auto x : g[cur]){
+        if(!vis[x]){
+            dfs(x, cur);
+            child++;
+            low[cur] = min(low[cur], low[x]);
+            if(low[x] > dis[cur]){
+                bridges.push_back({cur, x});
+            }
+            if(par != 0 && low[x] >= dis[cur]){
+                art_points.insert(cur);
+            }
+         
+        }else if ( x != par){
+            low[cur] = min(low[cur], dis[x]);
+        }
+    }
+    if(par == 0 && child > 1)art_points.insert(cur);
+}
+
+int32_t main(){
+     ios_base::sync_with_stdio(0);
+      cin.tie(0); 
+      #ifndef ONLINE_JUDGE 
+      freopen("input.txt","r",stdin); 
+      freopen("output.txt","w",stdout); 
+      #endif
+      auto start=clock();
+      int n, m;
+      cin >> n >> m;
+      for(int i = 0; i < m; i++){
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+      }
+      dfs(1, 0);
+      cout << "Articulation Bridges " << '\n';
+      for(auto x : bridges){
+        cout << x.first << "-- >" << x.second << '\n';
+      }
+      cout << "Articulation Points " << '\n';
+      for(auto x = art_points.begin(); x != art_points.end(); x ++){
+         cout << *x << '\n';
+      }
+      cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
+      return 0;
+}
+
