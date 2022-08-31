016bde2c61c2a71adf41a4b913b3da9888144b22
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-09-01 00:53:53 +0600

    Update Inversion Count( D & C).cpp

--- Algorithm/Inversion Count( D & C).cpp
+++ Algorithm/Inversion Count( D & C).cpp
@@ -1,51 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int merge_(int l, int mid, int h, vector<int> &ar){
-    vector<int> left, right;
-    for(int i = l; i <= mid; i++){
-        left.push_back(ar[i]);
-    }
-     for(int i = mid + 1; i <= h; i++){
-        right.push_back(ar[i]);
-    }
-   int i = 0, j = 0, k = l, cnt = 0;
-   while(i < left.size() && j < right.size()){
-      if(left[i] <= right[j]){
-        ar[k++] = left[i++];
-      }else{
-        cnt += (mid - l + 1);
-        ar[k++] = right[j++];
-      }
-   }
-   while(i < left.size()){
-    ar[k++] = left[i++];
-   }
-    while(j < right.size()){
-    ar[k++] = right[j++];
-   }
-return cnt;
+int merge(vector<int> &ar, int s, int e){
+    int i = s, mid = (s + e) >> 1, j = mid + 1, cnt = 0;
+     vector<int> tmp;
+     while(i <= mid and j <= e){
+        if(ar[i] <= ar[j]){
+            tmp.push_back(ar[i]);
+            i++;
+        }else{
+          cnt += (mid - i + 1);
+          tmp.push_back(ar[j]);
+          j++;
+        }
+     }
+     while(i <= mid){
+        tmp.push_back(ar[i++]);
+     }
+     while(j <= e){
+        tmp.push_back(ar[j++]);
+     }
+     int k = 0;
+     for(int idx = s; idx <= e; idx++){
+        ar[idx] = tmp[k++];
+     }
+     return cnt;
 }
 
-int INV_CNT(int l, int h, vector<int> &ar){
-    if(l >= h)return 0;
-    int mid = (l + h) >> 1;
-    int c1 = INV_CNT(l, mid, ar);
-    int c2 =  INV_CNT(mid + 1, h, ar);
-    int c3 =  merge_(l, mid, h, ar);
-    return c1 + c2 + c3;
+int inv_cnt(vector<int> &ar, int s, int e){
+    if(s >= e)return 0;
+    int mid = (s + e) >> 1;
+    int c1 = inv_cnt(ar, s, mid);
+    int c2 = inv_cnt(ar, mid + 1, e);
+    int combine = merge(ar, s, e);
+    return c1 + c2 + combine;
 }
 
 int32_t main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int n;
-    cin >> n;
-    vector<int> ar(n);
-    for(int i = 0; i < n; i++){
+     ios_base::sync_with_stdio(0);
+      cin.tie(0); 
+      #ifndef ONLINE_JUDGE 
+      freopen("input.txt","r",stdin); 
+      freopen("output.txt","w",stdout); 
+      #endif
+      auto start=clock();
+      int n;
+      cin >> n;
+      vector<int> ar(n);
+      for(int i = 0; i < n; i++){
         cin >> ar[i];
-    }
-    int ans = INV_CNT(0, n - 1, ar);
-    cout << ans << '\n';
-    return 0;
+      }
+      cout << inv_cnt(ar, 0, n - 1) << '\n';
+      cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
+      return 0;
 }
