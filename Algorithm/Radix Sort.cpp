bbab0f716fe2fb0d23b8072ea9213161123b4b1d
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-08-31 10:17:55 +0600

    Update Radix Sort.cpp

--- Algorithm/Radix Sort.cpp
+++ Algorithm/Radix Sort.cpp
@@ -49,3 +49,55 @@ int32_t main(){
     }
     return 0;
 }
+
+
+// Radix Sort Using Vector 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void counting_sort(vector<int> &ar, int n, int d){
+    vector<int> container(10, 0), output(n);
+    for(int i = 0; i < n; i++){
+       container[(ar[i] / d) % 10]++;
+    }
+    for(int i = 1; i < 10; i++){
+        container[i] += container[i - 1];
+    }
+    for(int i = n - 1; i >= 0; i--){
+        output[--container[(ar[i] / d ) % 10]] = ar[i];
+    }
+    for(int i = 0; i < n; i++){
+     ar[i] = output[i];
+   }
+
+}
+
+void radix_sort(vector<int> &ar, int n){
+    int mx = *max_element(ar.begin(), ar.end());
+    int d = 1;
+    while(mx / d){
+        counting_sort(ar, n, d);
+        d *= 10;
+    }
+}
+
+int32_t main(){
+     ios_base::sync_with_stdio(0);
+      cin.tie(0); 
+      #ifndef ONLINE_JUDGE 
+      freopen("input.txt","r",stdin); 
+      freopen("output.txt","w",stdout); 
+      #endif
+      auto start=clock();
+      int n;
+      cin >> n;
+      vector<int> ar(n);
+      for(int i = 0; i < n; i++){
+        cin >> ar[i];
+      }
+      radix_sort(ar, n);
+      for(int i = 0; i < n; i++)cout << ar[i] << ' ';
+      cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
+      return 0;
+}
