7c43e8475e9f6757cdc4cd3967a03e49a13016ea
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-09-09 09:26:52 +0600

    Update Prim's Algorithm (MST).cpp

--- Graph/Prim's Algorithm (MST).cpp
+++ Graph/Prim's Algorithm (MST).cpp
@@ -1,47 +1,60 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N=105;
-vector<pair<int,int>>G[N];
-bool vis[N];
 
-int MST(){
-	priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-	pq.push({0,0});int ans=0;
-	while(!pq.empty()){
-		pair<int,int>cur=pq.top();
-         int v=cur.second;
-         pq.pop();
-         if(!vis[v]){
-         	vis[v]=true;
-         	ans+=cur.first;
-         	for(auto it:G[v]){
-         		if(!vis[it.second]){
-         			pq.push(it);
-         		}
-         	}
-         }
-	}
-	return ans;
-}
+class graph{
+   int V;
+   vector<pair<int, int>> *l;
+
+   public:
+    graph(int n){
+        V = n;
+        l = new vector<pair<int, int>>[n];
+    }
+
+    void addEdge(int u, int v, int w){
+        l[u].push_back({v, w});
+        l[v].push_back({u, w});
+    }
+
+    int prims(){
+        priority_queue<pair<int, int> , vector<pair<int, int>> , greater<pair<int, int>>> pq;
+        bool *vis = new bool[V]{0};
+
+        int ans = 0;
+        pq.push({0, 1});
+
+        while (!pq.empty()){
+            auto best = pq.top();
+            pq.pop();
+            int weight = best.first;
+            int node = best.second;
+
+           
+            if(vis[node])continue;
+             ans += weight;
+             vis[node] = true;
+
+            for(auto x : l[node]){
+                 if(!vis[x.first]){
+                    pq.push({x.second, x.first});
+                 }
+            }
+        }
+         return ans;
+    }
+};
 
 int32_t main(){
-	 ios_base::sync_with_stdio(0);
- 	 cin.tie(0); 
- 	 cout.tie(0);
- 	 #ifndef ONLINE_JUDGE 
- 	 freopen("input.txt","r",stdin); 
- 	 freopen("output.txt","w",stdout); 
- 	 #endif
- 	 auto start=clock();
-      int n,e;
-      cin>>n>>e;
-      for(int i=0;i<e;i++){
-      	int x,y,wt;
-      	cin>>x>>y>>wt;
-      	G[x].push_back({wt,y});
-      	G[y].push_back({wt,x});
-      }
-      cout<<MST()<<endl;
- 	 cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
- 	 return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m;
+    cin >> n >> m;
+    graph g(n + 1);
+    for(int i = 0; i < m; i++){
+        int u, v, w;
+        cin >> u >> v >> w;
+        g.addEdge(u, v, w);
+    }
+    cout << g.prims() << '\n';
+    return 0;
 }
