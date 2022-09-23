8d9bc1de07c86f3970bf4085ae3fd2fecc37cf4a
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-09-23 12:42:36 +0600

    Update Diameter of a Tree.cpp

--- Graph/Diameter of a Tree.cpp
+++ Graph/Diameter of a Tree.cpp
@@ -2,49 +2,45 @@
 using namespace std;
 
 const int N = 1e5 + 9;
-vector<int> gp[N];
+vector<int> g[N];
 vector<int> depth(N, 0);
-int node, mx, dia;
 
 void dfs(int cur, int par){
-    depth[cur] = depth[par] + 1;
-    if(depth[cur] > mx){
-        mx = depth[cur];
-        node = cur;
-    }
-    for(auto x : gp[cur]){
+    for(auto x : g[cur]){
         if(x != par){
+            depth[x] = depth[cur] + 1;
             dfs(x, cur);
-        }
-    }
-}
-void dfs1(int cur, int par){
-    depth[cur] = depth[par] + 1;
-    dia = max(dia, depth[cur] - 1);
-    for(auto x : gp[cur]){
-        if(x != par){
-            dfs1(x, cur);
         }
     }
 }
 
 int32_t main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int n;
-    cin >> n;
-    for(int i = 1; i <= n; i++)gp[i].clear(), depth[i] = 0;
-    for(int i = 0 ; i < n - 1; i++){
+     ios_base::sync_with_stdio(0);
+      cin.tie(0); 
+      #ifndef ONLINE_JUDGE 
+      freopen("input.txt","r",stdin); 
+      freopen("output.txt","w",stdout); 
+      #endif
+      auto start=clock();
+      int n;
+      cin >> n;
+      for(int i = 0; i < n - 1; i++){
         int u, v;
         cin >> u >> v;
-        gp[u].push_back(v);
-        gp[v].push_back(u);
-    }
-    node = 0, mx = 0;
-    dfs(1, 0);
-    for(int i = 1; i <= n; i++)depth[i] = 0;
-     dia = 0;
-    dfs1(node, 0);
-    cout << dia << '\n';
-    return 0;
+        g[u].push_back(v);
+        g[v].push_back(u);
+      }
+      dfs(1, 0); int node = -1;
+      for(int i = 1; i <= n; i++){
+        if(node < depth[i])node = i;
+        cout << depth[i] << ' ';
+      }
+      cout << '\n';
+      for(int i = 0; i <= n; i++)depth[i] = 0;
+      dfs(node, 0);
+      for(int i = 1; i <= n; i++){
+        cout << depth[i] << ' ';
+      }
+      cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
+      return 0;
 }
