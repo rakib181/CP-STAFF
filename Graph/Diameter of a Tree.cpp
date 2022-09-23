fedcd3425cc68cff6b74dfa10ab10786c6c233b3
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-09-23 21:14:22 +0600

    Update Diameter of a Tree.cpp

--- Graph/Diameter of a Tree.cpp
+++ Graph/Diameter of a Tree.cpp
@@ -1,11 +1,11 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int N = 1e5 + 9;
+const int N = 2e5 + 9;
 vector<int> g[N];
 vector<int> depth(N, 0);
 
-void dfs(int cur, int par){
+void dfs(int cur, int par = 0){
     for(auto x : g[cur]){
         if(x != par){
             depth[x] = depth[cur] + 1;
@@ -15,32 +15,25 @@ void dfs(int cur, int par){
 }
 
 int32_t main(){
-     ios_base::sync_with_stdio(0);
-      cin.tie(0); 
-      #ifndef ONLINE_JUDGE 
-      freopen("input.txt","r",stdin); 
-      freopen("output.txt","w",stdout); 
-      #endif
-      auto start=clock();
-      int n;
-      cin >> n;
-      for(int i = 0; i < n - 1; i++){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    for(int i = 0; i < n - 1; i++){
         int u, v;
         cin >> u >> v;
         g[u].push_back(v);
         g[v].push_back(u);
-      }
-      dfs(1, 0); int node = -1;
-      for(int i = 1; i <= n; i++){
-        if(node < depth[i])node = i;
-        cout << depth[i] << ' ';
-      }
-      cout << '\n';
-      for(int i = 0; i <= n; i++)depth[i] = 0;
-      dfs(node, 0);
-      for(int i = 1; i <= n; i++){
-        cout << depth[i] << ' ';
-      }
-      cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
-      return 0;
+    }
+    dfs(1);
+    int node = 0, mx = INT_MIN;
+    for(int i = 1; i <= n; i++){
+        if(depth[i] > mx)mx = depth[i], node = i;
+        depth[i] = 0;
+    }
+    dfs(node); mx = INT_MIN;
+    for(int i = 1; i <= n; i++)if(depth[i] > mx)mx = depth[i];
+    cout << mx << '\n';
+    return 0;
 }
+
