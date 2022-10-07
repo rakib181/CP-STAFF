1130af4180731bd5a65f07abf51a392cab453953
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-10-08 00:59:53 +0600

    Create Postorder Traversal.cpp

---
+++ Recursion/Postorder Traversal.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    function<void(int)> P_O = [&](int n){
+        if(n <= 2){
+            if(n == 1 || n == 2)cout << n << ' ';
+            return;
+        }
+        P_O(n - 2);
+        P_O(n - 1);
+        cout << n << ' ';
+     };
+     P_O(n);
+    return 0;
+}
