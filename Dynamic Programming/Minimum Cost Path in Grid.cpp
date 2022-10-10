2a57127c85315a4433e067a5212aee5d3ea58afb
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-10-11 02:53:56 +0600

    Create Minimum Cost Path in Grid.cpp

---
+++ Dynamic Programming/Minimum Cost Path in Grid.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int min_path_cost(int i, int j, vector<vector<int>> &ar, int n, int m, vector<vector<int>> &dp){
+    if(i > n or j > m)return INT_MAX;
+    if(i == n and j == m)return ar[i][j];
+    if(dp[i][j] != -1)return dp[i][j];
+    return dp[i][j] = ar[i][j] + min(min_path_cost(i + 1, j, ar, n, m, dp), min_path_cost(i, j + 1, ar, n, m, dp));
+}
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> ar(n + 1, vector<int> (m + 1));
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            cin >> ar[i][j];
+        }
+    }
+    vector<vector<int>> dp(n + 1, vector<int> (m + 1, -1));
+    cout << min_path_cost(1, 1,ar, n, m, dp) << '\n';
+    return 0;
+}
