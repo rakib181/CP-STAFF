66d41c778980c08ec252324e1c3516b57a5a6f12
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-10-20 20:29:50 +0600

    Update Travelling Sellsman Problem.cpp

--- Dynamic Programming/Travelling Sellsman Problem.cpp
+++ Dynamic Programming/Travelling Sellsman Problem.cpp
@@ -1,18 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int tsp(vector<vector<int>> &dist, int setOfCity, int city, int n){
+int tsp(vector<vector<int>> &dist, int setOfCity, int city, int n, vector<vector<int>> &dp){
     if(setOfCity == (1 << n) - 1){
         return dist[city][0];
+    }
+    if(dp[setOfCity][city] != -1){
+        return dp[setOfCity][city];
     }
     int ans = INT_MAX;
     for(int choice = 0; choice < n; choice++){
         if(!(setOfCity & (1 << choice))){
-            int sub = dist[city][choice] + tsp(dist, (setOfCity | (1 << choice)), choice, n);
+            int sub = dist[city][choice] + tsp(dist, (setOfCity | (1 << choice)), choice, n, dp);
             ans = min(ans, sub);
         }
     }
-    return ans;
+    return dp[setOfCity][city] = ans;
 }
 
 
@@ -21,6 +24,7 @@ int  main(){
             {0, 20, 42, 25}, {20, 0, 30, 34},
             {42, 30, 0, 10}, {25, 34, 10, 0}
     };
-    cout << tsp(dist, 1, 0, 4) << '\n';
+    vector<vector<int>> dp((1 << dist.size()), vector<int> (dist.size(), -1));
+    cout << tsp(dist, 1, 0, dist.size(), dp) << '\n';
     return 0;
 }
