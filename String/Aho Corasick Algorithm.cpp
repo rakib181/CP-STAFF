50ed322a0f91499a7d80a01368498e3390e61b81
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-11-13 15:41:30 +0600

    Update Aho Corasick Algorithm.cpp

--- String/Aho Corasick Algorithm.cpp
+++ String/Aho Corasick Algorithm.cpp
@@ -1,13 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MX = 500, MXC = 27;
+#define int long long int
+const int MX = 500 * 500 + 10, MXC = 26;
 vector<vector<int>> Goto(MX, vector<int> (MXC, -1));
-vector<int> failure(MX, -1), output(MX, 0);
+vector<int> failure(MX, -1);
+bitset<510> output[MX];
+struct {
+   int occ;
+   char s[MX];
+}ar[510];
 
-void build_automata(string ar[], int n) {
+void build_automata(int n) {
     int st = 1;
     for (int i = 0; i < n; i++) {
-        string word = ar[i];
+        string word = ar[i].s;
         int cur_state = 0;
         for (char j: word) {
             int ch = j - 'a';
@@ -16,7 +22,7 @@ void build_automata(string ar[], int n) {
             }
             cur_state = Goto[cur_state][ch];
         }
-        output[cur_state] |= (1 << i);
+        output[cur_state].set(i + 1);
     }
     for (int i = 0; i < MXC; i++) {
         if (Goto[0][i] == -1) {
@@ -25,7 +31,7 @@ void build_automata(string ar[], int n) {
     }
     queue<int> q;
     for (int i = 0; i < MXC; i++) {
-        if (Goto[0][i] != 0) {
+        if (Goto[0][i] != 0 and Goto[0][i] != -1) {
             failure[Goto[0][i]] = 0;
             q.push(Goto[0][i]);
         }
@@ -37,7 +43,6 @@ void build_automata(string ar[], int n) {
             if (Goto[state][i] != -1) {
                 int f = failure[state];
                 while (Goto[f][i] == -1) f = failure[f];
-
                 f = Goto[f][i];
                 failure[Goto[state][i]] = f;
                 output[Goto[state][i]] |= output[f];
@@ -55,31 +60,40 @@ int get_next_state(int present_state, char ch){
     return Goto[tmp_state][i];
 }
 
-void search_word(string ar[], int n, string &text){
-    build_automata(ar, n);
+void search_word(int n, string &text){
+    build_automata(n);
     int present_state = 0;
-    for(int i = 0; i < (int) text.size(); i++){
-        present_state = get_next_state(present_state, text[i]);
-        if(!output[present_state])continue;
+    for(char i : text){
+        present_state = get_next_state(present_state, i);
+        if(output[present_state] == 0)continue;
             for(int j = 0; j < n; j++){
-                if(output[present_state] & (1 << j)){
-                    cout << "Word " << ar[j] << " location " << i - (int) ar[j].size() + 1 << '\n';
-                }
+               if(output[present_state].test(j + 1))ar[j].occ++;
             }
+    }
+    for(int i = 0; i < n; i++){
+        cout << ar[i].occ << '\n';
     }
 }
 
 int32_t main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n;
-    cin >> n;
-    string ar[n];
-    for(int i = 0; i < n; i++){
-        cin >> ar[i];
+    int tt, idx = 1;
+    cin >> tt;
+    while (tt--){
+        cout << "Case " << idx++ << ":" << '\n';
+        Goto.assign(MX, vector<int> (MXC, -1));
+        failure.assign(MX, -1);
+        memset(output, 0, MX);
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        for(int i = 0; i < n; i++){
+            cin >> ar[i].s;
+            ar[i].occ = 0;
+        }
+        search_word(n, s);
     }
-    string s;
-    cin >> s;
-    search_word(ar,n, s);
     return 0;
 }
