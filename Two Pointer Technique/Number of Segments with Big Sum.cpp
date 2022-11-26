dc492b3e78d1d66d1615d0383f29b2124c2b6046
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-11-27 00:41:45 +0600

    Create Number of Segments with Big Sum.cpp

---
+++ Two Pointer Technique/Number of Segments with Big Sum.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, s;
+    cin >> n >> s;
+    vector<int> ar(n + 1);
+    for(int i = 1; i <= n; i++){
+        cin >> ar[i];
+    }
+    int ans = 0, sum = 0, l = 1;
+    for(int r = 1; r <= n; r++){
+        sum += ar[r];
+        while (sum >= s){
+            ans += (n - r + 1);
+            sum -= ar[l++];
+        }
+    }
+    cout << ans << '\n';
+
+    return 0;
+}
