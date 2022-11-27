aec8776bc2b6b1a0f904059b0ba33ceafbe1a487
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-11-27 18:07:39 +0600

    Update Segments with Small Set.cpp

--- Two Pointer Technique/Segments with Small Set.cpp
+++ Two Pointer Technique/Segments with Small Set.cpp
@@ -27,3 +27,47 @@ int32_t main(){
 
     return 0;
 }
+
+// ANOTHER WAY
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+const int N = 1e5 + 9;
+vector<int> cnt(N, 0);
+int num = 0, k;
+
+void ADD(int x){
+   cnt[x]++;
+   if(cnt[x] == 1){
+       num++;
+   }
+}
+void REMOVE(int x){
+   cnt[x]--;
+   if(!cnt[x])num--;
+}
+bool GOOD(){
+    return  num <= k;
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n >> k;
+    vector<int> ar(n + 1);
+    for(int i = 1; i <= n; i++){
+        cin >> ar[i];
+    }
+    int l = 1, ans = 0;
+    for(int r = 1; r <= n; r++){
+        ADD(ar[r]);
+        while (!GOOD()){
+            REMOVE(ar[l++]);
+        }
+        ans += r - l + 1;
+    }
+    cout << ans << '\n';
+
+    return 0;
+}
