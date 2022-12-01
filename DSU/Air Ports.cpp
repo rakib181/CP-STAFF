273dcc83bc8464f9f8b60f50a814abb5255df1c1
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-12-01 11:27:04 +0600

    Create Air Ports.cpp

---
+++ DSU/Air Ports.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+class DSU{
+public:
+    vector<int> P, SZ;
+    void init(int n){
+        P.resize(n);
+        SZ.resize(n, 1);
+        iota(P.begin(), P.end(), 0);
+    }
+
+    int FIND(int v){
+        if(P[v] == v)return v;
+        return P[v] = FIND(P[v]);
+    }
+
+    void UNION(int u, int v){
+        u = FIND(u), v = FIND(v);
+        if(u == v)return;
+
+        if(SZ[u] > SZ[v]){
+            swap(u, v);
+        }
+        P[v] = u;
+        SZ[u] += SZ[v];
+    }
+}DS;
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt, idx = 1;
+    cin >> tt;
+    while (tt--) {
+        cout << "Case " << idx++ << ": ";
+        int n, m, c;
+        cin >> n >> m >> c;
+        DS.init(n * 2 + 5);
+        vector<pair<int, pair<int, int>>> edges;
+        for(int i = 0; i < m; i++){
+            int u, v, wt;
+            cin >> u >> v >> wt;
+            if(wt >= c)continue;
+            edges.push_back({wt, {u, v}});
+        }
+        sort(edges.begin(), edges.end());
+        int tot = 0;
+        for(auto x : edges){
+            int u = x.second.first, v = x.second.second, wt = x.first;
+            if(DS.FIND(u) != DS.FIND(v)){
+                tot += wt;
+                DS.UNION(u, v);
+            }
+        }
+        int cc = 0;
+        for(int i = 1; i <= n; i++){
+            if(DS.FIND(i) == i)cc++;
+        }
+        cout << tot + (cc * c) << ' ' << cc <<'\n';
+    }
+    return 0;
+}
