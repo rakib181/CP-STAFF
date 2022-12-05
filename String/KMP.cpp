525ad53a2c26264164d4e8ff0fbe495735224728
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-12-05 18:11:02 +0600

    Update KMP.cpp

--- String/KMP.cpp
+++ String/KMP.cpp
@@ -1,40 +1,28 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-
+void prefix(string &s, vector<int> &pi){
+   pi[0] = -1; int n = (int) s.size() ;
+   for(int i = 1; i <= n; i++){
+       int k = pi[i - 1];
+       while (k >= 0 and s[k] != s[i - 1])k = pi[k];
+       pi[i] = k + 1;
+   }
+}
 int32_t main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    string s, pattern;
-    cin >> s >> pattern;
-    int n = pattern.size();
-    vector<int> lps(n + 1);
-    auto _lps = [&](string s){
-        int i = 0, j = 1, idx = 1;
-        lps[0] = 0;
-        for( ;s[j];  ){
-            if(s[i] == s[j]){
-                lps[idx++] = i + 1;
-                i++, j++;
-            }else{
-                if(i != 0)i = lps[i - 1];
-                else j++, lps[idx++] = 0;
-            }
+    string s, pat;
+    cin >> s >> pat;
+    int nn = (int) pat.size();
+    pat += "$" + s;
+    int n = (int) pat.size();
+    vector<int> pi(n + 1);
+    prefix(pat, pi);
+    for(int i = 0; i <= n; i++){
+        if(pi[i] == nn){
+            cout << i - 2 * nn << ' ';
         }
-    };
-    _lps(pattern);
-     int i = 0, j = 0, cnt = 0;
-     while(i < s.size()){
-         if(s[i] == pattern[j]){
-             i++, j++;
-         }else{
-             if(j != 0)j = lps[j - 1];
-             else i++;
-         }
-         if(j == pattern.size()){
-            cnt++;
-         }
-     }
-   cout << cnt << '\n';
+    }
     return 0;
 }
