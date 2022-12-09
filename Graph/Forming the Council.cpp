60489f515a0ae22c8cd679e6496b81e01e34cf95
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-12-09 18:37:21 +0600

    Update Forming the Council.cpp

--- Graph/Forming the Council.cpp
+++ Graph/Forming the Council.cpp
@@ -95,3 +95,92 @@ int32_t main() {
     }
    return 0;
 }
+
+// Another Approach
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+const int N = 1e5 + 9;
+vector<int> g[N], rev_g[N], top;
+bool vis[N];
+
+void dfs(int cur){
+    vis[cur] = true;
+    for(auto x: g[cur]){
+        if(!vis[x]){
+            dfs(x);
+        }
+    }
+    top.push_back(cur);
+}
+int comp[N];
+void scc(int cur, int cc){
+    comp[cur] = cc;
+    for(auto x : rev_g[cur]){
+        if(comp[x] == -1){
+            scc(x, cc);
+        }
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt, cs = 1;
+    cin >> tt;
+    while (tt--){
+        cout << "Case " << cs++ << ": ";
+        int m, n;
+        cin >> m >> n;
+        top.clear();
+        for(int i = 1; i < N; i++){
+            g[i].clear();
+            rev_g[i].clear();
+            comp[i] = -1;
+            vis[i] = false;
+        }
+        for(int i = 1; i <= m; i++){
+            int u, v; char ch, chh;
+            cin >> ch >> u >> chh >> v;
+            if(ch == '-') u = 2 * n - u + 1;
+            if(chh == '-') v = 2 * n - v + 1;
+            g[2 * n - u + 1].push_back(v);
+            g[2 * n - v + 1].push_back(u);
+            rev_g[u].push_back(2 * n - v + 1);
+            rev_g[v].push_back(2 * n - u + 1);
+        }
+        for(int i = 1; i <= 2 * n; i++){
+            if(!vis[i]){
+                dfs(i);
+            }
+        }
+        int cc = 0;
+        reverse(top.begin(), top.end());
+        for(int i = 0; i < (int) top.size(); i++){
+            if(comp[top[i]] == -1){
+                scc(top[i], cc++);
+            }
+        }
+        bool ok = true;
+        for(int i = 1; i <= n; i++){
+            if(comp[i] == comp[2 * n - i + 1]){
+                ok = false;
+                break;
+            }
+        }
+        if(ok){
+            cout << "Yes" << '\n';
+            vector<int> ans;
+            for(int i = 1; i <= n; i++){
+                if(comp[i] > comp[2 * n - i + 1])ans.push_back(i);
+            }
+            cout << (int) ans.size() << ' ';
+            for(auto x : ans){
+                cout << x << ' ';
+            }
+            cout << '\n';
+        }else cout << "No" << '\n';
+    }
+    return 0;
+}
