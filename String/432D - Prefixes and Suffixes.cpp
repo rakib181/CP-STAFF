3bfe0773678885b997caf236fd5e561b3284e7df
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-12-11 10:20:02 +0600

    Update 432D - Prefixes and Suffixes.cpp

--- String/432D - Prefixes and Suffixes.cpp
+++ String/432D - Prefixes and Suffixes.cpp
@@ -1,3 +1,4 @@
+// Z Algorithm
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long int
@@ -39,6 +40,42 @@ int32_t main() {
     cout << (int) ans.size() << '\n';
     for(auto x : ans){
         cout << x.first << ' ' << x.second << '\n';
+    }
+    return 0;
+}
+
+// KMP
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+void prefix(string &s, vector<int> &pi){
+    pi[0] = -1; int n = (int) s.size() ;
+    for(int i = 1; i <= n; i++){
+        int k = pi[i - 1];
+        while (k >= 0 and s[k] != s[i - 1])k = pi[k];
+        pi[i] = k + 1;
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s;
+    cin >> s;
+    int n = (int) s.size();
+    vector<int> pi(n + 1), f(n + 1, 0);
+    prefix(s, pi);
+    for(int i = n; i > 0; i--){
+        f[pi[i]] += ++f[i];
+    }
+    vector<pair<int, int>> ans;
+    for(int i = n; i > 0; i = pi[i]){
+        ans.emplace_back(i, f[i]);
+    }
+    cout << (int) ans.size() << '\n';
+    for(auto it = ans.rbegin(); it != ans.rend(); it++){
+        cout << it -> first << ' ' << it -> second << '\n';
     }
     return 0;
 }
