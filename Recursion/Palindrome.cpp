ee867bc8c03938420d89301ca47f888617b0c341
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-12-15 18:45:41 +0600

    Update Palindrome.cpp

--- Recursion/Palindrome.cpp
+++ Recursion/Palindrome.cpp
@@ -27,3 +27,25 @@ int32_t main(){
      cout << palindrome(s, 0, s.size() - 1);
     return 0;
 }
+
+// Another Way
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+bool palindrome(string s){
+    if((int) s.size() == 0 or (int) s.size() == 1)return true;
+    if(s[0] == s[s.size() - 1]){
+        return palindrome(s.substr(1, s.size() - 2));
+    }
+    return false;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s;
+    getline(cin, s);
+    cout << palindrome(s) << '\n';
+    return 0;
+}
