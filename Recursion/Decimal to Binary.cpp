c32ae7684735589489c2aaa5c7104d8cf8ea27b3
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-12-15 18:59:11 +0600

    Update Decimal to Binary.cpp

--- Recursion/Decimal to Binary.cpp
+++ Recursion/Decimal to Binary.cpp
@@ -17,3 +17,23 @@ int32_t main() {
     DTB(n);
     return 0;
 }
+//Another Way
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+string DTB(int x, string res){
+    if(x == 0)return res;
+    if(x & 1) res = "1" + res;
+    else res = "0" + res;
+    return DTB(x / 2, res);
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    cout << DTB(n, "") << '\n';
+    return 0;
+}
