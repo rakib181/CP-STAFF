e5aea87376c7d885e6f318a8fc41e4597dde9037
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-12-15 18:39:58 +0600

    Update Reverse a String(Rec).cpp

--- Recursion/Reverse a String(Rec).cpp
+++ Recursion/Reverse a String(Rec).cpp
@@ -15,3 +15,21 @@ int32_t main() {
     cout << rev(s, (int) s.size() - 1) << '\n';
     return 0;
 }
+// Another Way
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+string rev(string s){
+   if(s.empty())return "";
+    return rev(s.substr(1)) + s.front();
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s;
+    getline(cin, s);
+    cout << rev(s) << '\n';
+    return 0;
+}
