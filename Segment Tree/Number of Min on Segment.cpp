ca74d93150d787892651495786de6d61b4bf0c84
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-12-31 13:13:59 +0600

    Create Number of Min on Segment.cpp

---
+++ Segment Tree/Number of Min on Segment.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+struct item{
+    int m, cnt;
+};
+
+struct segment{
+    int sz;
+    vector<item> a;
+    void init(int n){
+        sz = 1;
+        while (sz < n)sz *= 2;
+        a.assign(2 * sz, {0, 0});
+    }
+    item merge(item x, item y){
+        if(x.m < y.m)return x;
+        else if(x.m > y.m)return y;
+        else return {x.m, x.cnt + y.cnt};
+    }
+    void build(vector<int> &aa, int x, int lx, int rx){
+        if(rx - lx == 1){
+            if(lx < (int) aa.size()){
+                a[x] = {aa[lx], 1};
+            }
+            return;
+        }
+        int m = (lx + rx) / 2;
+        build(aa, 2 * x + 1, lx, m);
+        build(aa, 2 * x + 2, m, rx);
+        a[x] = merge(a[2 * x + 1] , a[2 * x + 2]);
+    }
+    void build(vector<int> &aa){
+        build(aa, 0, 0, sz);
+    }
+    void set(int i, int v, int x, int lx, int rx){
+        if(rx - lx == 1){
+            a[x] = {v, 1};
+            return;
+        }
+        int m = (lx + rx) / 2;
+        if(i < m){
+            set(i, v, 2 * x + 1, lx, m);
+        }else{
+            set(i, v, 2 * x + 2, m, rx);
+        }
+        a[x] = merge(a[2 * x + 1] , a[2 * x + 2]);
+    }
+    void set(int i, int v){
+        set(i, v, 0, 0, sz);
+    }
+    item query(int l, int r, int x, int lx, int rx){
+        if(lx >= r or l >= rx)return {INT_MAX, INT_MAX};
+        if(lx >= l and rx <= r)return a[x];
+        int m = (lx + rx) / 2;
+        return merge(query(l, r, 2 * x + 1, lx, m) , query(l, r, 2 * x + 2, m, rx));
+    }
+    item query(int l, int r){
+        return query(l, r, 0, 0, sz);
+    }
+};
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m;
+    cin >> n >> m;
+    segment st;
+    st.init(n);
+    vector<int> a(n);
+    for(int i = 0; i < n; i++){
+      cin >> a[i];
+    }
+    st.build(a);
+    while (m--){
+        int type;
+        cin >> type;
+        if(type == 1){
+            int i, v;
+            cin >> i >> v;
+            st.set(i, v);
+        }else{
+            int l, r;
+            cin >> l >> r;
+            item ans = st.query(l, r);
+            cout << ans.m << ' ' << ans.cnt << '\n';
+        }
+    }
+    return 0;
+}
