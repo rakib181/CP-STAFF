e062f4290c02a69cf5f9edd5731a9b01b420adab
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2023-01-01 13:17:29 +0600

    Create Intersecting Segments.cpp

---
+++ Segment Tree/Intersecting Segments.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+struct segment{
+    int sz{};
+    vector<int> a;
+    void init(int n){
+        sz = 1;
+        while (sz < n) sz *= 2;
+        a.assign(2 * sz, 0);
+    }
+    void set(int i, int v, int x, int lx, int rx){
+        if(rx - lx == 1){
+            a[x] = v;
+            return;
+        }
+        int m = (lx + rx) / 2;
+        if(i < m){
+            set(i, v, 2 * x + 1, lx, m);
+        }else{
+            set(i, v, 2 * x + 2, m, rx);
+        }
+        a[x] = a[2 * x + 1] + a[2 * x + 2];
+    }
+    void set(int i, int v){
+        set(i, v, 0, 0, sz);
+    }
+    int query(int l, int r, int x, int lx, int rx){
+        if(lx >= r or l >= rx)return 0;
+        if(lx >= l and rx <= r)return a[x];
+        int m = (lx + rx) / 2;
+        return query(l, r, 2 * x + 1, lx, m) + query(l, r, 2 * x + 2, m, rx);
+    }
+    int query(int l, int r){
+        return query(l, r, 0, 0, sz);
+    }
+};
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    segment st;
+    int n;
+    cin >> n; st.init(2 * n);
+    vector<int> a(2 * n), pos(n, -1), ans(n);
+    for(int i = 0; i < 2 * n; i++){
+        cin >> a[i];
+        a[i]--;
+        if(pos[a[i]] == -1){
+            pos[a[i]] = i;
+            st.set(pos[a[i]], 1);
+        }else{
+            st.set(pos[a[i]], 0);
+            ans[a[i]] = st.query(pos[a[i]], i);
+        }
+    }
+    st.init(2 * n);
+    reverse(a.begin(), a.end());
+    pos.assign(n, -1);
+    for(int i = 0; i < 2 * n; i++){
+        if(pos[a[i]] == -1){
+            pos[a[i]] = i;
+            st.set(pos[a[i]], 1);
+        }else{
+            st.set(pos[a[i]], 0);
+            ans[a[i]] += st.query(pos[a[i]], i);
+        }
+    }
+    for(int i = 0; i < n; i++){
+        cout << ans[i] << ' ';
+    }
+    return 0;
+}
