f308e442cef41f5731e7d3cdbc1d59edc1d5789a
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2023-01-13 02:06:46 +0600

    Update Aho Corasick Algorithm.cpp

--- String/Aho Corasick Algorithm.cpp
+++ String/Aho Corasick Algorithm.cpp
@@ -1,99 +1,88 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define int long long int
-const int MX = 500 * 500 + 10, MXC = 26;
-vector<vector<int>> Goto(MX, vector<int> (MXC, -1));
-vector<int> failure(MX, -1);
-bitset<510> output[MX];
-struct {
-   int occ;
-   char s[MX];
-}ar[510];
 
-void build_automata(int n) {
-    int st = 1;
-    for (int i = 0; i < n; i++) {
-        string word = ar[i].s;
+const int NX = 505, MXS = NX * NX + 2;
+string text;    int n;
+vector<string> inp(NX);
+vector<int> failure, ans;
+vector<vector<int>> trie;
+bitset<NX> out[MXS];
+
+void BuildAutomata(){
+    failure.assign(MXS, -1);
+    trie.assign(MXS, vector<int> (30, -1));
+    memset(out, 0, sizeof out);
+    int state = 0;
+    for(int i = 0; i < n; i++){
         int cur_state = 0;
-        for (char j: word) {
-            int ch = j - 'a';
-            if (Goto[cur_state][ch] == -1) {
-                Goto[cur_state][ch] = st++;
-            }
-            cur_state = Goto[cur_state][ch];
+        for(int j = 0; j < (int) inp[i].size(); j++){
+              int c = inp[i][j] - 'a';
+              if(trie[cur_state][c] == -1){
+                  trie[cur_state][c] = ++state;
+              }
+              cur_state = trie[cur_state][c];
         }
-        output[cur_state].set(i + 1);
+        out[cur_state].set(i);
     }
-    for (int i = 0; i < MXC; i++) {
-        if (Goto[0][i] == -1) {
-            Goto[0][i] = 0;
-        }
+    for(int i = 0; i < 26; i++){
+        if(trie[0][i] == -1)trie[0][i] = 0;
     }
     queue<int> q;
-    for (int i = 0; i < MXC; i++) {
-        if (Goto[0][i] != 0 and Goto[0][i] != -1) {
-            failure[Goto[0][i]] = 0;
-            q.push(Goto[0][i]);
+    for(int i = 0; i < 26; i++){
+        if(trie[0][i] != -1 and trie[0][i] != 0){
+            failure[trie[0][i]] = 0;
+            q.push(trie[0][i]);
         }
     }
-    while (!q.empty()) {
-        int state = q.front();
+    while (!q.empty()){
+        int st = q.front();
         q.pop();
-        for (int i = 0; i < MXC; i++) {
-            if (Goto[state][i] != -1) {
-                int f = failure[state];
-                while (Goto[f][i] == -1) f = failure[f];
-                f = Goto[f][i];
-                failure[Goto[state][i]] = f;
-                output[Goto[state][i]] |= output[f];
-                q.push(Goto[state][i]);
+        for(int i = 0; i < 26; i++){
+            if(trie[st][i] != -1){
+                int fail = failure[st];
+                while (trie[fail][i] == -1)fail = failure[fail];
+                fail = trie[fail][i];
+                failure[trie[st][i]] = fail;
+                out[trie[st][i]] |= out[fail];
+                q.push(trie[st][i]);
             }
         }
     }
 }
 
-int get_next_state(int present_state, char ch){
-    int tmp_state = present_state, i = ch - 'a';
-    while(Goto[tmp_state][i] == -1){
-        tmp_state = failure[tmp_state];
-    }
-    return Goto[tmp_state][i];
-}
-
-void search_word(int n, string &text){
-    build_automata(n);
-    int present_state = 0;
-    for(char i : text){
-        present_state = get_next_state(present_state, i);
-        if(output[present_state] == 0)continue;
-            for(int j = 0; j < n; j++){
-               if(output[present_state].test(j + 1))ar[j].occ++;
-            }
-    }
-    for(int i = 0; i < n; i++){
-        cout << ar[i].occ << '\n';
-    }
+int find_next_state(int cur_state, int now){
+    while (trie[cur_state][now] == -1) cur_state = failure[cur_state];
+    return trie[cur_state][now];
 }
 
 int32_t main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    int tt, idx = 1;
+    int tt, cs = 1;
     cin >> tt;
     while (tt--){
-        cout << "Case " << idx++ << ":" << '\n';
-        Goto.assign(MX, vector<int> (MXC, -1));
-        failure.assign(MX, -1);
-        memset(output, 0, MX);
-        int n;
+        ans.assign(NX, 0);
         cin >> n;
-        string s;
-        cin >> s;
+        cin >> text;
         for(int i = 0; i < n; i++){
-            cin >> ar[i].s;
-            ar[i].occ = 0;
+            cin >> inp[i];
         }
-        search_word(n, s);
+        BuildAutomata();
+        int current_state = 0;
+        for(char i : text){
+            int now = i - 'a';
+            current_state = find_next_state(current_state, now);
+            if(out[current_state] == 0) continue;
+            for(int j = 0; j < n; j++){
+                 if(out[current_state].test(j)){
+                     ans[j]++;
+                 }
+            }
+        }
+        cout << "Case " << cs++ << ":\n";
+        for(int i = 0; i < n; i++){
+            cout << ans[i] << '\n';
+        }
     }
     return 0;
 }
