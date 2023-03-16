cb02154e48d781b8189dd7980c980327a1031f7b
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2023-03-17 01:28:48 +0600

    Update Removing Digits.cpp

--- Dynamic Programming/Removing Digits.cpp
+++ Dynamic Programming/Removing Digits.cpp
@@ -19,3 +19,32 @@ int32_t main(){
     cout << dp[n] << '\n';
     return 0;
 }
+
+\\ Recursive Approach
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+const int N = 1e6 + 9, MOD = 1e9 + 7;
+int dp[N], n;
+
+int yo(int i){
+    if(i == 0)return 0;
+    if(dp[i] != -1)return dp[i];
+    int ans = INT_MAX;
+    string s = to_string(i);
+    for(auto x : s){
+       if((x - '0' >= 1) and (i - (x - '0') >= 0)) ans = min(ans, yo(i - (x - '0')) + 1);
+    }
+    return dp[i] = ans;
+}
+
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n;
+    memset(dp, -1, sizeof dp);
+    yo(n);
+    cout << dp[n] << '\n';
+    return 0;
+}
