cc61c014d8383c17402ddee5ebfe39c84d0485b8
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2023-03-17 01:10:13 +0600

    Update Dice Combinations.cpp

--- Dynamic Programming/Dice Combinations.cpp
+++ Dynamic Programming/Dice Combinations.cpp
@@ -19,3 +19,32 @@ int32_t main(){
     cout << dp[n] << '\n';
     return 0;
 }
+
+// Recursive Approach
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+const int N = 1e6 + 9, MOD = 1e9 + 7;
+int dp[N], n;
+
+int comb(int i){
+    if(i < 0)return 0;
+    if(i == 0) return 1;
+    if(dp[i] != -1)return dp[i];
+    int ans = 0;
+    for(int j = 1; j <= 6; j++){
+        ans = ans + comb(i - j);
+        ans %= MOD;
+    }
+    return dp[i] = ans;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n;
+    memset(dp, -1, sizeof dp);
+    comb(n);
+    cout << dp[n] << '\n';
+    return 0;
+}
