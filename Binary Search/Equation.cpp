eb50c27d2c982fab2b5a5129b49b20435247960d
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2023-03-31 01:27:47 +0600

    Update Equation.cpp

--- Binary Search/Equation.cpp
+++ Binary Search/Equation.cpp
@@ -49,3 +49,39 @@ int32_t main(){
     cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
     return 0;
 }
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+const double eps = 1e-6; double c;
+
+double SQRT(double x){
+    double l = 1, r = x / 2 + 1.0;
+    while ((r - l) > eps){
+        double m = (l + r) / 2;
+        if(m * m >= x){
+            r = m;
+        }else l = m;
+    }
+    return r;
+}
+
+bool good(double x){
+    return (x * x) + SQRT(x) >= c;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> c;
+    double l = 1, r = 1e5, ans = 0;
+    while ((r - l) > eps){
+        double m = (l + r) / 2;
+        if(good(m)){
+            ans = m;
+            r = m;
+        }else l = m;
+    }
+    cout << fixed << setprecision(10) << ans << '\n';
+    return 0;
+}
