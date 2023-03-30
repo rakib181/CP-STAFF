e7ca6d83d883df2fe37d576571fb4832f19e09b1
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2023-03-30 21:59:19 +0600

    Update Put N Rectangles in a Minimum Square .cpp

--- Binary Search/Put N Rectangles in a Minimum Square .cpp
+++ Binary Search/Put N Rectangles in a Minimum Square .cpp
@@ -31,3 +31,29 @@ int32_t main(){
     cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
     return 0;
 }
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int w, h, n;
+    cin >> w >> h >> n;
+
+    auto good = [&] (int m){
+        int a = m / w;
+        int b = m / h;
+        return a >= (double) n / b;
+    };
+    int l = 1, r = 1e14, ans = 0;
+    while (l <= r){
+        int m = l + (r - l) / 2;
+        if(good(m)){
+            ans = m;
+            r = m - 1;
+        }else l = m + 1;
+    }
+    cout << ans << '\n';
+    return 0;
+}
