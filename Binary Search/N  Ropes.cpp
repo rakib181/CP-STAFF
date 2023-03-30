c814e8aa1f7a4f844290a77bca154697c06e2293
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2023-03-30 22:13:22 +0600

    Update N  Ropes.cpp

--- Binary Search/N  Ropes.cpp
+++ Binary Search/N  Ropes.cpp
@@ -37,3 +37,35 @@ int32_t main(){
   cerr<<1.0 * (clock()-start)/CLOCKS_PER_SEC<<endl;
   return 0;
 }
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+const int N = 1e5 + 9; double eps = 1e-6;
+double a[N];
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, k;
+    cin >> n >> k;
+    for(int i = 1; i <= n; i++){
+        cin >> a[i];
+    }
+    auto good = [&](double m){
+        int cnt = 0;
+        for(int i = 1; i <= n; i++){
+            cnt += a[i] / m;
+        }
+        return cnt >= k;
+    };
+    double l = 0.0, r = accumulate(a + 1, a + n + 1, 0.0);
+    while((r - l) > eps){
+        double m = (l + r) / 2.0;
+        if(good(m)){
+            l = m;
+        }else r = m;
+    }
+    cout << fixed << setprecision(10) << r << '\n';
+    return 0;
+}
