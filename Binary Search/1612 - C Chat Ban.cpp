35b0d34f0894db3f8e03ebb2dacbf9937a0efea8
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2023-04-01 11:38:59 +0600

    Update 1612 - C Chat Ban.cpp

--- Binary Search/1612 - C Chat Ban.cpp
+++ Binary Search/1612 - C Chat Ban.cpp
@@ -27,3 +27,45 @@ int32_t main(){
     }
     return 0;
 }
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+int k, x;
+
+int get_sum(int n){
+    return n * (n + 1) / 2;
+}
+
+int get_range_sum(int l, int r){
+    return get_sum(r) - get_sum(l - 1);
+}
+bool good(int i){
+    int s;
+    if(i <= k){
+        s = get_range_sum(1, i);
+    }else{
+        s = get_range_sum(1, k) + get_range_sum(2 * k - i, k - 1);
+    }
+    return s >= x;
+}
+
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt;
+    cin >> tt;
+    while(tt--) {
+       cin >> k >> x;
+       int l = 1, r = 2 * k - 1, ans = 2 * k - 1;
+       while(l <= r){
+           int m = (l + r) >> 1;
+           if(good(m)){
+               ans = m;
+               r = m - 1;
+           }else l = m + 1;
+       }
+       cout << ans << '\n';
+    }
+    return 0;
+}
