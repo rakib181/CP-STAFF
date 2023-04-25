fcfe56b45aaa35b8374070ca50e0eb6d8b5c1fa7
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2023-04-26 01:26:48 +0600

    Update 21. Ant Colony.cpp

--- Segment Tree/Academy Problem/21. Ant Colony.cpp
+++ Segment Tree/Academy Problem/21. Ant Colony.cpp
@@ -86,3 +86,77 @@ signed main() {
 	}
 	return 0;
 }
+
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const int N = 1e5 + 9, inf = 1e9 + 7;
+int a[N];
+
+struct seg {
+	struct node {
+		int GCD, MN, MN_CNT;
+	};
+	node t[4 * N];
+	node merge(node x, node y) {
+		node ans;
+		ans.GCD = __gcd(x.GCD, y.GCD);
+		ans.MN = min(x.MN, y.MN);
+		if (x.MN < y.MN) {
+			ans.MN_CNT = x.MN_CNT;
+		} else if (x.MN > y.MN) {
+			ans.MN_CNT = y.MN_CNT;
+		} else {
+			ans.MN_CNT = x.MN_CNT + y.MN_CNT;
+		}
+		return ans;
+	}
+	void build(int n, int b, int e) {
+		if (b == e) {
+			t[n].GCD = a[b];
+			t[n].MN = a[b];
+			t[n].MN_CNT = 1;
+			return;
+		}
+		int m = (b + e) >> 1;
+		build(2 * n, b, m);
+		build(2 * n + 1, m + 1, e);
+		t[n] = merge(t[2 * n], t[2 * n + 1]);
+	}
+	node query(int n, int b, int e, int l, int r) {
+		if (b > r or e < l)return {0, inf, 0};
+		if (b >= l and e <= r)return t[n];
+		int m = (b + e) >> 1;
+		return merge(query(2 * n, b, m, l, r), query(2 * n + 1, m + 1, e, l, r));
+	}
+} s;
+
+
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+	s.build(1, 1, n);
+	int q;
+	cin >> q;
+	while (q--) {
+		int l, r;
+		cin >> l >> r;
+		auto x = s.query(1, 1, n, l, r);
+		int ans = 0;
+		if (x.GCD == x.MN) {
+			ans = x.MN_CNT;
+		}
+		cout << (r - l + 1) - ans << '\n';
+	}
+	return 0;
+}
