3aa6ffb2e39374262383849d82bd357511ed55fa
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2023-05-14 12:06:13 +0600

    Update 12. Ordering Tasks.cpp

--- Graph/Academy Problem/12. Ordering Tasks.cpp
+++ Graph/Academy Problem/12. Ordering Tasks.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
-const int N = 1e5 + 9;
+const int N = 105;
 vector<int> g[N];
 int ind[N];
 
@@ -34,6 +34,54 @@ signed main() {
                     q.push(x);
                 }
             }
+        }
+        cout << '\n';
+    }
+    return 0;
+}
+
+//DFS
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const int N = 105;
+vector<int> g[N];
+bool vis[N];
+vector<int> order;
+
+void dfs(int u){
+    vis[u] = true;
+    for(auto x : g[u]){
+        if(!vis[x]){
+            dfs(x);
+        }
+    }
+    order.emplace_back(u);
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m;
+    while(cin >> n >> m and !(n == 0 and m == 0)) {
+        order.clear();
+        for(int i = 1; i <= n; i++){
+            g[i].clear();
+            vis[i] = false;
+        }
+        for(int i = 1; i <= m; i++){
+            int u, v;
+            cin >> u >> v;
+            g[u].emplace_back(v);
+        }
+        for(int i = 1; i <= n; i++){
+            if(!vis[i]){
+                dfs(i);
+            }
+        }
+        reverse(order.begin(), order.end());
+        for(auto  x: order){
+            cout << x << ' ';
         }
         cout << '\n';
     }
