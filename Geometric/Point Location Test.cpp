98576aad357e2291b2b075a31391a9012c2da0df
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2024-05-02 00:32:30 +0600

    Update Point Location Test.cpp

--- Geometric/Point Location Test.cpp
+++ Geometric/Point Location Test.cpp
@@ -21,3 +21,45 @@ signed main(){
     }
     return 0;
 }
+
+//Better Solution
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+struct PT{
+    int x, y;
+    void read(){
+        cin >> x >> y;
+    }
+    PT operator - (PT &other) const{
+        return {x - other.x, y - other.y};
+    }
+    void operator -= (PT &other){
+        x -= other.x;
+        y -= other.y;
+    }
+    ll cross_product(PT &other) const{
+        return 1LL * x * other.y - 1LL * y * other.x;
+    }
+};
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--) {
+        PT p1{}, p2{}, p3{};
+        p1.read();
+        p2.read();
+        p3.read();
+        p2 -= p1;
+        p3 -= p1;
+        ll prod = p2.cross_product(p3);
+        if(prod < 0)cout << "RIGHT\n";
+        else if(prod > 0)cout << "LEFT\n";
+        else cout << "TOUCH\n";
+    }
+    return 0;
+}
