d3e09891e8240e4ed676062a6ca5ea66f97e1059
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2024-05-14 12:14:47 +0600

    Update Tree Diameter.cpp

--- Tree/Tree Diameter.cpp
+++ Tree/Tree Diameter.cpp
@@ -36,3 +36,62 @@ int32_t main(){
     cout << mx << '\n';
     return 0;
 }
+
+// Tree Diameter 
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const int N = 2e5 + 9;
+vector<int> g[N];
+int toLeaf[N], maxLength[N];
+
+void dfs(int u, int p){
+    for(auto v : g[u]){
+        if(v != p){
+            dfs(v, u);
+        }
+    }
+    for(auto v : g[u]){
+        if(v != p){
+            toLeaf[u] = max(toLeaf[u], toLeaf[v] + 1);
+        }
+    }
+}
+
+void cal(int u, int p){
+    vector<int> d;
+    for(auto v : g[u]){
+        if(v != p){
+            cal(v, u);
+            d.emplace_back(toLeaf[v]);
+        }
+    }
+    sort(d.rbegin(), d.rend());
+    if((int) d.size() == 1){
+        maxLength[u] = max(maxLength[u], 1 + d[0]);
+    }else if((int) d.size() >= 2){
+        maxLength[u] = max(maxLength[u], 2 + d[0] + d[1]);
+    }
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    for(int i = 1; i < n; i++){
+        int u, v;
+        cin >> u >> v;
+        g[u].emplace_back(v);
+        g[v].emplace_back(u);
+    }
+    dfs(1, 0);
+    cal(1, 0);
+    int ans = 0;
+    for(int i = 1; i <= n; i++){
+        ans = max(ans, maxLength[i]);
+    }
+    cout << ans << '\n';
+    return 0;
+}
